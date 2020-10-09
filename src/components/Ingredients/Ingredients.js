cb280821b6bd662d77d06fd483ac66fc76ddbeb3
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-10-09 18:26:06 +1100

    remove ingredients from the list

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -12,13 +12,23 @@ const Ingredients = () => {
       { id: Math.random().toString(), ...ingredient },
     ]);
   };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
